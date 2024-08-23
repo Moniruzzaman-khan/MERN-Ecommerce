83d5031f408e58b5b5664aca055220c8dfd63b41
Moniruzzaman-khan <61977327+Moniruzzaman-khan@users.noreply.github.com>
2024-08-23 08:55:36 +0600

    Update EmailHelper.js

--- src/utility/EmailHelper.js
+++ src/utility/EmailHelper.js
@@ -1,25 +1,20 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const path = require('path');
 
 const EmailSend= async (EmailTo, EmailText, EmailSubject) => {
 
     let transporter = nodemailer.createTransport({
+        service: 'gamil',
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
         auth: {
-            type: "OAuth2",
-            user: "19monirkhan19@gmail.com",
-            clientId: "217454359693-l0bbn3an5efbqq2v140tvtectomlj6ld.apps.googleusercontent.com",
-            clientSecret: "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",
-            refreshToken: "1//04svkQlVx9vgVCgYIARAAGAQSNwF-L9IrfgLHMmaRIjD7_pTkOYB3OSWnDkYJOE9vRTxxXFXZwLcLpx2nyZ6JbySxA74AFJlxxZ8",
-            accessToken: "ya29.a0AXooCgtFe-rRqwVBie6-xYh4af3_U4vzhmf4oDUyH-25B-HUBqIQ8JmxpoQ2BEGL04VkSzinhYKdiCTVuaM_glcYrWGBmttEv9QHpcXt5RSyECDIM1SU_sgcrcPBc_bI2-3-E7-qRAmmqMHdRJ4G3MvMjvKYjo29CSM3aCgYKAf0SARASFQHGX2MisEvFXIZ0jSE9ilMVu_rMLg0171",
-            expires: 999999999,
+            user: "25monirkhan25@gmail.com",
+            pass: "lrjl vkrn zojg stta"
         },
     });
 
 
     let mailOptions = {
-        from: 'Monir Mart <19monirkhan19@gmail.com>',
+        from: 'Monir Mart <25monirkhan25@gmail.com>',
         to: EmailTo,
         subject: EmailSubject,
         text: EmailText
